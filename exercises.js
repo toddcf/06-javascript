d0f82b26592079a20f577af302d17c560a3d105b
Todd Croak-Falen <toddcf@gmail.com>
2017-11-01 23:51:08 -0700

    Did age calculator exercise.

--- exercises.js
+++ exercises.js
@@ -6,10 +6,16 @@
 
 // JS Stalker Exercise
 
-var firstName = prompt( "What is your first name?" );
-console.log(firstName);
-var lastName = prompt( "What is your last name?" );
-console.log(lastName);
-var age = prompt( "What is your age?" );
-console.log(age);
-alert( "Your name is " + firstName + " " + lastName + ", and you are " + age + " years old." );
+// var firstName = prompt( "What is your first name?" );
+// console.log(firstName);
+// var lastName = prompt( "What is your last name?" );
+// console.log(lastName);
+// var age = prompt( "What is your age?" );
+// console.log(age);
+// alert( "Your name is " + firstName + " " + lastName + ", and you are " + age + " years old." );
+
+// Age Calculator Exercise
+
+var age = prompt( "Enter your age to find out how many days you've been alive (approximately)." );
+var days = age * 365;
+alert( "You have been alive for approximately " + days + " days!" );
